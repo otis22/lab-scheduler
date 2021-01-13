f2aa6908bccc0290705bc8a89d0d4d1a844f9496
otis22 <pro100print@yahoo.com>
2021-01-13 11:58:19 +0200

    fix for work remotely

--- jupyter.py
+++ jupyter.py
@@ -1,6 +1,6 @@
 import os
 c = get_config()
 c.NotebookApp.password = os.environ.get("JUPYTER_PASSWORD")
-c.NotebookApp.ip = '*'
+c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.open_browser = False
